59375d03b202b4c46473b6c7e8cd232474ef79e0
emmanuel <nwakwueemmanuel1@gmail.com>
2024-12-11 18:31:22 -0800

    new

--- components/Notifications/Sections/All.tsx
+++ components/Notifications/Sections/All.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
 
 const All = () => {
 
@@ -16,16 +16,38 @@ const All = () => {
         {
             name:"Activities"
         },
+        {
+            name:"Activities"
+        },
+        {
+            name:"Activities"
+        },
+        {
+            name:"Activities"
+        },
     ]
     return (
-        <View>
-           <View>
-            
+        <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+           <View style={{flexDirection:"row", gap:5, paddingHorizontal:10, paddingVertical:20}}>
+            {Notifics.map((_items,index)=>(
+                <View key={index}>
+                    <View style={styles.box}>
+                        <Text style={{fontFamily:"Proxima"}}>{_items.name}</Text>
+                    </View>
+                </View>
+        ))}
             </View> 
-        </View>
+        </ScrollView>
     );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    box:{
+        paddingVertical:10,
+        paddingHorizontal:14,
+        borderWidth:1,
+        borderColor:"#000"
+    }
+})
 
 export default All;
