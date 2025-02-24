05364c725524c7c75e267d76f5b8ff5f5c58c36c
emmanuel <nwakwueemmanuel1@gmail.com>
2025-02-24 07:51:29 -0800

    m

--- components/CheckOut/Selections/Payments/Ussd.tsx
+++ components/CheckOut/Selections/Payments/Ussd.tsx
@@ -1,17 +1,17 @@
 import React, { useEffect, useState } from "react";
-import { StyleSheet, Text, TouchableOpacity, View , Modal,
+import {  Text, TouchableOpacity, View , Modal,
     TextInput,
     FlatList,   Animated,
     ScrollView,} from 'react-native';
 import DropdownUssD from '../../AddModal/SelectionPayment/UssDModal/USSDModal';
-import YourCart from '@/styles/Cart/YourCart.styles';
-import { Ionicons } from '@expo/vector-icons';
+ import { Ionicons } from '@expo/vector-icons';
 import ArrowUpGray from "@/assets/svg/ArrowUpGray";
 import ArrowGrayDown from "@/assets/svg/ArrowGrayDown";
 import Colon from "@/assets/svg/colon";
 import Search from "@/assets/images/kyc/Search";
- import BackButton from "@/assets/images/kyc/LeftArrow";
-import LeftButton from "@/assets/images/cart/arrowLeftMain";
+ import LeftButton from "@/assets/images/cart/arrowLeftMain";
+import UssdStyles from "@/styles/UssdStyles/Ussdstyles";
+import YourCart from "@/styles/Cart/YourCart.styles";
 
 type Bank = {
     name: string;
@@ -92,21 +92,21 @@ const Ussd = () => {
     
       const renderDigitBoxes = (digits: string[]) =>
         digits.map((digit, index) => (
-          <View key={index} style={styles.digitBox}>
-            <Text style={styles.digitText}>{digit}</Text>
+          <View key={index} style={UssdStyles.digitBox}>
+            <Text style={UssdStyles.digitText}>{digit}</Text>
           </View>
         ));
     
     return (
-        <View style={styles.container}>
-      <View style={styles.header}>
-        <Text style={[YourCart.title, styles.title]}>Select Bank</Text>
+        <View style={UssdStyles.container}>
+      <View style={UssdStyles.header}>
+        <Text style={[YourCart.title, UssdStyles.title]}>Select Bank</Text>
         <Text style={YourCart.bonusText}>
           Select your bank to initiate USSD payment. Transfer from your own bank account.
         </Text>
       </View>
       {/* Dropdown for USSD Payment */}
-      <View style={styles.dropdown}>
+      <View style={UssdStyles.dropdown}>
       <TouchableOpacity
         onPress={() => setModalVisible(true)}
         style={{width:"90%"}}
@@ -122,9 +122,9 @@ const Ussd = () => {
       </TouchableOpacity>
       </View>
       {/* <DropdownUssD /> */}
-      <Animated.View style={[styles.dropdowns, { transform: [{ scaleY }] }]}>
+      <Animated.View style={[UssdStyles.dropdowns, { transform: [{ scaleY }] }]}>
       {isOpen && (
-  <View style={styles.dropdownContainer}>
+  <View style={UssdStyles.dropdownContainer}>
     <ScrollView showsVerticalScrollIndicator={false}>
       {selectedBank && (
         <View style={YourCart.UssdModal}>
@@ -137,14 +137,14 @@ const Ussd = () => {
           <Text style={YourCart.smallTxt}>
             Dial the code and complete payment within the next
           </Text>
-          <View style={styles.timerContainer}>
+          <View style={UssdStyles.timerContainer}>
             <View style={{ flexDirection: 'column' }}>
               <View style={{ flexDirection: 'row' }}>
                 {renderDigitBoxes(minutesArray)}
               </View>
               <Text>Minutes</Text>
             </View>
-            <View style={styles.colon}>
+            <View style={UssdStyles.colon}>
               <Colon />
             </View>
             <View style={{ flexDirection: 'column' }}>
@@ -163,19 +163,19 @@ const Ussd = () => {
       </Animated.View>
       {/* Modal dropdown */}
       <Modal visible={modalVisible} animationType="slide" transparent>
-        <View style={styles.modalOverlay}>
-          <View style={styles.modalContent}>
+        <View style={UssdStyles.modalOverlay}>
+          <View style={UssdStyles.modalContent}>
              {/* Modal Header */}
             <View style={[YourCart.rewardInfoContainer,{width:"90%"}]}>
               <TouchableOpacity >
                <LeftButton />
                </TouchableOpacity>
              {/* Convert totalAmount to string */}
-              <Text style={styles.headerText}>Select Bank</Text>
+              <Text style={UssdStyles.headerText}>Select Bank</Text>
             </View>
-           <View style={styles.dropdowned}>
+           <View style={UssdStyles.dropdowned}>
            <TextInput
-              style={styles.searchInput}
+              style={UssdStyles.searchInput}
               placeholder="Search"
               value={searchQuery}
               onChangeText={setSearchQuery}
@@ -183,7 +183,7 @@ const Ussd = () => {
             <Search/>
            </View>
              <TouchableOpacity
-              style={styles.dropdown}
+              style={UssdStyles.dropdown}
               onPress={() => setDropdownVisible(!dropdownVisible)}
             >
              <Text style={[YourCart.bonusText,{color:'#A4A4A4'}]}>
@@ -199,7 +199,7 @@ const Ussd = () => {
               keyExtractor={(item) => item.name}
               renderItem={({ item }) => (
                 <TouchableOpacity
-                  style={styles.bankItem}
+                  style={UssdStyles.bankItem}
                   onPress={() => handleSelectBank(item)}
                 >
                   <Text style={[YourCart.rewardText,{fontWeight:"400",color:"#212121s"}]}>{item.name}</Text>
@@ -214,191 +214,5 @@ const Ussd = () => {
     </View>
     );
 }
-const styles = StyleSheet.create({
-    container: {
-      gap: 5,
-      paddingBottom: 10,
-      paddingHorizontal: 10,
-      paddingVertical: 10,
-      borderWidth: 1,
-      borderColor: '#E9E9E9',
-      borderRadius: 4,  
-      backgroundColor: '#FFFFFF',  
-    },
-    headerText: {
-        marginHorizontal:"auto",
-        fontSize: 19,
-       fontWeight: '700',
-       color: '#212121',
-        marginTop: 13,
-       fontFamily:"Helvetica Neue",
-       paddingBottom:14,
-       lineHeight:26.6
-     },
-    dropdownButton: {
-        width: "100%",
-        padding: 10,
-        borderWidth: 1,
-        borderColor: "#E9E9E9",
-        borderRadius: 8,
-        backgroundColor: "#f8f8f8",
-        alignItems: "center",
-      },
-      dropdownButtonText: {
-        fontSize: 16,
-        color: "#333",
 
-      },
-    header: {
-      paddingBottom: 5,
-    },
-    title: {
-      fontSize: 16,
-      // fontWeight: '600',  
-      // color: '#333333',  
-    },
-    dropdown: {
-        flexDirection: "row",
-        justifyContent: "space-between",
-        alignItems: "center",
-        padding: 15,
-        borderWidth: 1,
-        borderColor: "#E9E9E9",
-        borderRadius: 6,
-        backgroundColor: "#FFFFFF",
-      },
-      dropdowned: {
-        flexDirection: "row",
-        justifyContent: "space-between",
-        alignItems: "center",
-        paddingHorizontal: 15,
-        borderWidth: 1,
-        borderColor: "#E9E9E9",
-        borderRadius: 6,
-        backgroundColor: "#FFFFFF",
-        marginVertical: 10,
-        paddingVertical:3,
-      },
-      dropdownText: {
-        fontSize: 16,
-        color: "#333",
-      },
-      modalOverlay: {
-        flex: 1,
-        justifyContent: "flex-end",
-        alignItems: "center",
-        backgroundColor: "rgba(0,0,0,0.5)",
-      },
-      modalContent: {
-        width: "100%",
-        backgroundColor: "white",
-        borderRadius: 10,
-        padding: 20,
-        elevation: 5,
-      },
-      searchInput: {
-        // borderWidth: 1,
-        borderColor: "#CBD5E0",
-        borderRadius: 6,
-        padding: 10,
-        marginVertical: 5,
-      },
-      bankItem: {
-        flexDirection: "row",
-        justifyContent: "space-between",
-        paddingVertical: 20,
-        paddingHorizontal:10
-       
-      },
-      bankName: {
-        fontSize: 16,
-        fontWeight: "500",
-      },
-      ussdCode: {
-        fontSize: 14,
-        color: "#666",
-      },
-      closeButton: {
-        marginTop: 10,
-        padding: 12,
-        backgroundColor: "#E53E3E",
-        borderRadius: 6,
-        alignItems: "center",
-      },
-      closeText: {
-        color: "#FFF",
-        fontSize: 16,
-        fontWeight: "600",
-      },
-      button: {
-        width: '100%',
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-        backgroundColor: '#fff',
-        paddingHorizontal: 13,
-        paddingVertical: 13,
-        borderTopLeftRadius: 4,
-        borderTopRightRadius: 4,
-        borderColor: '#E9E9E9',
-        borderWidth: 1,
-      },
-      buttonRow: {
-        flexDirection: 'row',
-        gap: 10,
-        alignItems: 'center',
-      },
-      dropdowns: {
-        overflow: 'hidden',
-      },
-      dropdownContainer: {
-        backgroundColor: '#fff',
-        width: '100%',
-        borderBottomLeftRadius: 4,
-        borderBottomRightRadius: 4,
-        borderLeftWidth: 1,
-        borderRightWidth: 1,
-        borderBottomWidth: 1,
-        borderColor: '#E9E9E9',
-      },
-      row: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        gap: 2,
-      },
-      saveText: {
-        color: '#009217',
-        fontWeight: '400',
-        fontSize: 12,
-        lineHeight: 16.8,
-        textAlign: 'right',
-      },
-      timerContainer: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'center',
-        marginTop: 20,
-        padding: 10,
-      },
-      digitBox: {
-        marginHorizontal:6,
-        paddingVertical:7,
-        paddingHorizontal:15,
-         borderWidth: 1,
-        borderColor: '#E9E9E9',
-        justifyContent: 'center',
-        alignItems: 'center',
-        borderRadius: 4,
-        backgroundColor: '#F5EADC',
-       },
-      colon: {
-       alignItems:"center",
-        marginBottom:"10%"
-      },
-      digitText: {
-        fontSize: 22,
-        fontWeight: '600',
-        color: '#333',
-      },
-  });
 export default Ussd;
